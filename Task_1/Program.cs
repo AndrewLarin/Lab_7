1d26f406daad036fb4d1df567b353a9b5e8e7712
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-10 22:59:56 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double z1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double z2 = Convert.ToDouble(Console.ReadLine());
+            if (Square(x1, y1, z1) > Square(x2, y2, z2))
+                Console.WriteLine("Площадь первого треугольника больше, чем второго");
+            else if ((Square(x1, y1, z1) < Square(x2, y2, z2)))
+                Console.WriteLine("Площадь первого треугольника меньше, чем второго");
+            else
+                Console.WriteLine("Площади треугольников равны");
+        }
+        static double Square(double x, double y, double z)
+        {
+            double p = (x + y + z) / 2;
+            return Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+        }
+
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Cube(a);
+        }
+        static void Cube(double a)
+        {
+            double v = a * a * a;
+            double s = 6 * a * a;
+            Console.WriteLine("Площадь поверности куба: {0}", s);
+            Console.WriteLine("Объем куба: {0}", v);
+
+        }
+    }
+}
